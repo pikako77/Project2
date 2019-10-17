5b9705e9864073967bd16f39b9500b695136dd18
ckuo21 <47082843+ckuo21@users.noreply.github.com>
2019-10-17 18:37:19 -0500

    Jessie's app.py update

--- Energy_Source/app.py
+++ Energy_Source/app.py
@@ -38,8 +38,24 @@ def index():
 
 @app.route("/dashboard")
 def dashboard():
-    """Return the homepage."""
+    
     return render_template("dashboard.html")
+
+@app.route("/future")
+def future():
+    
+    return render_template("future.html")
+
+@app.route("/now")
+def now():
+    
+    return render_template("now.html")    
+
+@app.route("/history")
+def history():
+    
+    return render_template("history.html")   
+
 
 @app.route("/data")
 def get_data():
@@ -52,7 +68,7 @@ def get_data():
   conn.close()
   return Alltypes_df.to_json(orient="records")
 
-@app.route("/data/year")
+@app.route("/year")
 def get_year():
     engine = create_engine("sqlite:///db/Alltypes.sqlite")
     conn = engine.connect()
@@ -62,18 +78,6 @@ def get_year():
     conn.close()
     # Return a list of the column names (sample names)
     return jsonify(list(data.columns)[2:])
-
-@app.route("/data/state")
-def get_state():
-    engine = create_engine("sqlite:///db/Alltypes.sqlite")
-    conn = engine.connect()
-  
-    sql = f"select * from Alltypes"
-    data = pd.read_sql(sql, conn)
-    conn.close()
-
-    # Return a list of the column names (sample names)
-    return jsonify(list(data["State"]))
 
 
 @app.route("/<energy_type>/<yr>")
@@ -103,8 +107,8 @@ def select_data(energy_type, yr):
     return data.to_json(orient="records")
     # return jsonify(data)
     
-@app.route("/data/<energy_type>/<state>")
-def select_data_per_state(energy_type,state):
+@app.route("/<energy_type>")
+def select_data_per_state(energy_type,state='AK'):
     
     engine = create_engine("sqlite:///db/Alltypes.sqlite")
     conn = engine.connect()
@@ -114,54 +118,11 @@ def select_data_per_state(energy_type,state):
     conn.close()
 
     tmp = data[data['Type'] == energy_type]
-    consumption_data_tmp = tmp[tmp['State']==state].iloc[0].tolist()
+    selected_data = tmp[tmp['State']==state]
 
-    # test_Data ={}
 
-    yr = []
-    consumption = []
-    print(consumption_data_tmp)
-    for i in range(2,len(data.columns)):
-        yr.append( (data.columns)[i])
-        consumption.append(consumption_data_tmp[i])
-        # print(selected_data.loc[0])
-        # print()
+    return selected_data.to_json(orient="records")
 
-    # print(yr)
-    
-    selected_data={
-        "consumption": consumption,
-        "yr": yr, 
-    }
-
-    # data = pd.DataFrame(selected_data)
-    
-    # return data.to_json(orient="records")
-    return jsonify(selected_data)
-
-@app.route("/energy_type/<state>/<yr>")
-def select_energyType_per_state_year(state,yr):
-    engine = create_engine("sqlite:///db/Alltypes.sqlite")
-
-    sql = f"select * from Alltypes"
-    
-    # result = engine.execute("sql statement")
-
-    conn = engine.connect()
-  
-    data = pd.read_sql(sql, conn)
-    conn.close()
-    tmp = data[data['State'] == state]
-    sel_data =tmp[yr].tolist()
-    # print(sel_data)
-    data_label =['Coal','NaturalGas','Petroleum','Nuclear','Renewable']
-    selected_data ={
-        "EnergyType": data_label, 
-        "consumption" : sel_data
-    }
-
-    # print(selected_data)
-    return jsonify(selected_data)
 
 if __name__ == "__main__":
-    app.run(port=5008, debug=True)
+    app.run(port=5012, debug=True)
